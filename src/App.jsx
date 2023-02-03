ba4f7c91eaf5648d051bbc91d6ca8307d3cbce65
Jorge Asuaje <jorge.asuaje@gmail.com>
2023-02-02 22:47:41 -0500

    Add winner logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import Board from './components/Board';
+import { useState } from 'react';
 import './styles.scss';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(squares);
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, index) => {
+        if (clickedPosition === index) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return squareValue;
+      });
+    });
+
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
   return (
     <div className="app">
-      <Board value="6" />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
